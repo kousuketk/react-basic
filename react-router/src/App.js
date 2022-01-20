fbfe464ac654cafb690093af2d8df3a39ba278e3
kousuke takahashi <pamarikomi_t@icloud.com>
2022-01-20 22:23:43 +0900

    created routes

--- react-router/src/App.js
+++ react-router/src/App.js
@@ -1,9 +1,33 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 function App() {
   return (
-    <div>
+    <BrowserRouter>
       <h1>Hello React Router</h1>
-    </div>
+      <Routes>
+        <Route exact path="/" element={<Home />}></Route>
+        <Route path="/about" element={<About />}></Route>
+        <Route path="/contact" element={<Contact />}></Route>
+        <Route path="*" element={<NotFound />}></Route>
+      </Routes>
+    </BrowserRouter>
   );
+}
+
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function About() {
+  return <h2>About</h2>;
+}
+
+function Contact() {
+  return <h2>Contact</h2>;
+}
+
+function NotFound() {
+  return <h2>Not Found Page</h2>;
 }
 
 export default App;
