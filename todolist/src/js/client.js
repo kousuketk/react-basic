637605326df81ee0e81762eb3cf4e73224ba121e
kousuke takahashi <pamarikomi_t@icloud.com>
2022-01-22 00:57:21 +0900

    add pages

--- todolist/src/js/client.js
+++ todolist/src/js/client.js
@@ -4,6 +4,8 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import Layout from "./pages/Layout";
 import Todos from "./pages/Todos";
+import Favorites from "./pages/Favorites";
+import Settings from "./pages/Settings";
 
 const app = document.getElementById("app");
 
@@ -11,6 +13,8 @@ ReactDOM.render(
   <Router>
     <Layout>
       <Route exact path="/" component={Todos}></Route>
+      <Route path="/favorites" component={Favorites}></Route>
+      <Route path="/settings" component={Settings}></Route>
     </Layout>
   </Router>,
   app
