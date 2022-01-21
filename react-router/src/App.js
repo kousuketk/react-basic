00bace30cc4e965c51941a76ee9f071e558a0437
kousuke takahashi <pamarikomi_t@icloud.com>
2022-01-21 21:45:05 +0900

    created state

--- react-router/src/App.js
+++ react-router/src/App.js
@@ -36,8 +36,8 @@ function App() {
         <Route exact path="/" element={<Home />}></Route>
         <Route path="/about" element={<About />}></Route>
         <Route path="/contact" element={<Contact />}></Route>
-        <Route path="/posts/:id" element={<PostId />}></Route>
         <Route path="/posts" element={<Posts />}></Route>
+        <Route path="/posts/:id" element={<PostId />}></Route>
         <Route path="*" element={<NotFound />}></Route>
       </Routes>
     </BrowserRouter>
@@ -60,14 +60,37 @@ function NotFound() {
   return <h2>Not Found Page</h2>;
 }
 
-function PostId() {
-  const params = useParams();
-  const id = params.id;
-  return <h2>Post List {id}</h2>;
-}
+const posts = [
+  { id: 1, title: "React", content: "React Tutorial" },
+  { id: 2, title: "Vue", content: "Vue.js Tutorial" },
+  { id: 3, title: "Laravel", content: "Laravel Tutorial" },
+];
 
 function Posts() {
-  return <h2>Post List</h2>;
+  return (
+    <div>
+      <h2>Post List</h2>
+      <ul>
+        {posts.map((post) => (
+          <li key={post.id}>
+            <NavLink to={`/posts/${post.id}`}>{post.title}</NavLink>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+function PostId() {
+  const params = useParams();
+  const id = Number(params.id);
+  const post = posts.find((post) => post.id === id);
+  return (
+    <div>
+      <h2>{post.title}</h2>
+      <p>{post.content}</p>
+    </div>
+  );
 }
 
 export default App;
