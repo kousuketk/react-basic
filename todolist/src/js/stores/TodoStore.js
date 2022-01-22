3377779abec154249ce5e80cbbed744d9c9491cc
kousuke takahashi <pamarikomi_t@icloud.com>
2022-01-23 00:54:20 +0900

    created dispatcher

--- todolist/src/js/stores/TodoStore.js
+++ todolist/src/js/stores/TodoStore.js
@@ -1,5 +1,7 @@
 import { EventEmitter } from "events";
 
+import dispatcher from "../dispatcher";
+
 class TodoStore extends EventEmitter {
   constructor() {
     super();
@@ -32,8 +34,17 @@ class TodoStore extends EventEmitter {
   getAll() {
     return this.todos;
   }
+
+  handleActions(action) {
+    switch (action.type) {
+      case "CREATE_TODO": {
+        this.createTodo(action.text);
+      }
+    }
+  }
 }
 
 const todoStore = new TodoStore();
+dispatcher.register(todoStore.handleActions.bind(todoStore));
 
 export default todoStore;
