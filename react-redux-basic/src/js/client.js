23bfd6a304d7a3c469eeb9066f74e68a2d0026a6
kousuke takahashi <pamarikomi_t@icloud.com>
2022-01-23 11:09:59 +0900

    created action

--- react-redux-basic/src/js/client.js
+++ react-redux-basic/src/js/client.js
@@ -1,7 +1,13 @@
 import { createStore } from "redux";
 
-const reducer = () => {
-  console.log("reducer has been called.");
+const reducer = (state = 0, action) => {
+  switch (action.type) {
+    case "INC":
+      return state + action.payload;
+    case "DEC":
+      return state - action.payload;
+  }
+  return state;
 };
 
 const store = createStore(reducer, 1);
@@ -10,4 +16,7 @@ store.subscribe(() => {
   console.log("store changed", store.getState());
 });
 
-store.dispatch({ type: "INC" });
+store.dispatch({ type: "INC", payload: 1 });
+store.dispatch({ type: "INC", payload: 10 });
+store.dispatch({ type: "INC", payload: 100 });
+store.dispatch({ type: "DEC", payload: 1000 });
